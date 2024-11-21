59fdce5ce50914439fd88e088c0699a0ea19b400
hw <410518072@qq.com>
2024-11-21 21:25:34 +0800

    h5部署在非根目录

--- config/index.js
+++ config/index.js
@@ -97,7 +97,7 @@ const config = {
     devServer: {
       hot: false,
     },
-    publicPath: ".",
+    publicPath: "/sunrise-app",
     staticDirectory: "static",
     postcss: {
       autoprefixer: {
